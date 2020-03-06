3d6a0d461d389a517e60a5a5b48d14521dd5d06b
bupticybee <icybee@bupt.edu.cn>
2020-03-06 15:14:32 +0800

    :new: 初步完成elephantfish主体代码

--- elephantfish.py
+++ elephantfish.py
@@ -8,7 +8,7 @@ from collections import namedtuple
 
 piece = { 'P': 44, 'N': 108, 'B': 23, 'R': 233, 'Q': 23, 'C': 101, 'K': 60000}
 
-# 子力价值表参考“梦入神蛋”
+# 子力价值表参考“象眼”
 
 pst = {
     "P": (
@@ -207,6 +207,17 @@ class Position(namedtuple('Position', 'board score')):
                     elif self.board[scanpos] != '.':
                         break
             if not p.isupper(): continue
+            if p == 'C':
+                for d in directions[p]:
+                    cfoot = 0
+                    for j in count(i+d, d):
+                        q = self.board[j]
+                        if q.isspace():break
+                        if cfoot == 0 and q == '.':yield (i,j)
+                        elif cfoot == 0 and q != '.':cfoot += 1
+                        elif cfoot == 1 and q.islower(): yield (i,j);break
+                        elif cfoot == 1 and q.isupper(): break;
+                continue
             for d in directions[p]:
                 for j in count(i+d, d):
                     q = self.board[j]
@@ -218,7 +229,7 @@ class Position(namedtuple('Position', 'board score')):
                     elif p in ('Q','K') and (j < 160 or j & 15 > 8 or j & 15 < 6): break
                     elif p == 'B' and j < 128: break
                     elif p == 'N':
-                        n_diff_x = abs((j - i) & 15)
+                        n_diff_x = (j - i) & 15
                         if n_diff_x == 14 or n_diff_x == 2:
                             if self.board[i + (1 if n_diff_x == 2 else -1)] != '.': break
                         else:
@@ -233,13 +244,11 @@ class Position(namedtuple('Position', 'board score')):
     def rotate(self):
         ''' Rotates the board, preserving enpassant '''
         return Position(
-            self.board[::-1].swapcase(), -self.score)
+            self.board[-2::-1].swapcase() + " ", -self.score)
 
     def nullmove(self):
         ''' Like rotate, but clears ep and kp '''
-        return Position(
-            self.board[::-1].swapcase(), -self.score,
-            self.bc, self.wc, 0, 0)
+        return self.rotate()
 
     def move(self, move):
         i, j = move
@@ -247,33 +256,11 @@ class Position(namedtuple('Position', 'board score')):
         put = lambda board, i, p: board[:i] + p + board[i+1:]
         # Copy variables and reset ep and kp
         board = self.board
-        wc, bc, ep, kp = self.wc, self.bc, 0, 0
         score = self.score + self.value(move)
         # Actual move
         board = put(board, j, board[i])
         board = put(board, i, '.')
-        # Castling rights, we move the rook or capture the opponent's
-        if i == A1: wc = (False, wc[1])
-        if i == H1: wc = (wc[0], False)
-        if j == A8: bc = (bc[0], False)
-        if j == H8: bc = (False, bc[1])
-        # Castling
-        if p == 'K':
-            wc = (False, False)
-            if abs(j-i) == 2:
-                kp = (i+j)//2
-                board = put(board, A1 if j < i else H1, '.')
-                board = put(board, kp, 'R')
-        # Pawn promotion, double move and en passant capture
-        if p == 'P':
-            if A8 <= j <= H8:
-                board = put(board, j, 'Q')
-            if j - i == 2*N:
-                ep = i + N
-            if j == self.ep:
-                board = put(board, j+S, '.')
-        # We rotate the returned position, so it's ready for the next player
-        return Position(board, score, wc, bc, ep, kp).rotate()
+        return Position(board, score).rotate()
 
     def value(self, move):
         i, j = move
@@ -282,21 +269,174 @@ class Position(namedtuple('Position', 'board score')):
         score = pst[p][j] - pst[p][i]
         # Capture
         if q.islower():
-            score += pst[q.upper()][119-j]
-        # Castling check detection
-        if abs(j-self.kp) < 2:
-            score += pst['K'][119-j]
-        # Castling
-        if p == 'K' and abs(i-j) == 2:
-            score += pst['R'][(i+j)//2]
-            score -= pst['R'][A1 if j < i else H1]
-        # Special pawn stuff
-        if p == 'P':
-            if A8 <= j <= H8:
-                score += pst['Q'][j] - pst['P'][j]
-            if j == self.ep:
-                score += pst['P'][119-(j+S)]
+            score += pst[q.upper()][255-j-1]
         return score
+
+###############################################################################
+# Search logic
+###############################################################################
+
+# lower <= s(pos) <= upper
+Entry = namedtuple('Entry', 'lower upper')
+
+class Searcher:
+    def __init__(self):
+        self.tp_score = {}
+        self.tp_move = {}
+        self.history = set()
+        self.nodes = 0
+
+    def bound(self, pos, gamma, depth, root=True):
+        """ returns r where
+                s(pos) <= r < gamma    if gamma > s(pos)
+                gamma <= r <= s(pos)   if gamma <= s(pos)"""
+        self.nodes += 1
+
+        # Depth <= 0 is QSearch. Here any position is searched as deeply as is needed for
+        # calmness, and from this point on there is no difference in behaviour depending on
+        # depth, so so there is no reason to keep different depths in the transposition table.
+        depth = max(depth, 0)
+
+        # Sunfish is a king-capture engine, so we should always check if we
+        # still have a king. Notice since this is the only termination check,
+        # the remaining code has to be comfortable with being mated, stalemated
+        # or able to capture the opponent king.
+        if pos.score <= -MATE_LOWER:
+            return -MATE_UPPER
+
+        # We detect 3-fold captures by comparing against previously
+        # _actually played_ positions.
+        # Note that we need to do this before we look in the table, as the
+        # position may have been previously reached with a different score.
+        # This is what prevents a search instability.
+        # FIXME: This is not true, since other positions will be affected by
+        # the new values for all the drawn positions.
+        if DRAW_TEST:
+            if not root and pos in self.history:
+                return 0
+
+        # Look in the table if we have already searched this position before.
+        # We also need to be sure, that the stored search was over the same
+        # nodes as the current search.
+        entry = self.tp_score.get((pos, depth, root), Entry(-MATE_UPPER, MATE_UPPER))
+        if entry.lower >= gamma and (not root or self.tp_move.get(pos) is not None):
+            return entry.lower
+        if entry.upper < gamma:
+            return entry.upper
+
+        # Here extensions may be added
+        # Such as 'if in_check: depth += 1'
+
+        # Generator of moves to search in order.
+        # This allows us to define the moves, but only calculate them if needed.
+        def moves():
+            # First try not moving at all. We only do this if there is at least one major
+            # piece left on the board, since otherwise zugzwangs are too dangerous.
+            if depth > 0 and not root and any(c in pos.board for c in 'RNC'):
+                yield None, -self.bound(pos.nullmove(), 1-gamma, depth-3, root=False)
+            # For QSearch we have a different kind of null-move, namely we can just stop
+            # and not capture anythign else.
+            if depth == 0:
+                yield None, pos.score
+            # Then killer move. We search it twice, but the tp will fix things for us.
+            # Note, we don't have to check for legality, since we've already done it
+            # before. Also note that in QS the killer must be a capture, otherwise we
+            # will be non deterministic.
+            killer = self.tp_move.get(pos)
+            if killer and (depth > 0 or pos.value(killer) >= QS_LIMIT):
+                yield killer, -self.bound(pos.move(killer), 1-gamma, depth-1, root=False)
+            # Then all the other moves
+            for move in sorted(pos.gen_moves(), key=pos.value, reverse=True):
+            #for val, move in sorted(((pos.value(move), move) for move in pos.gen_moves()), reverse=True):
+                # If depth == 0 we only try moves with high intrinsic score (captures and
+                # promotions). Otherwise we do all moves.
+                if depth > 0 or pos.value(move) >= QS_LIMIT:
+                    yield move, -self.bound(pos.move(move), 1-gamma, depth-1, root=False)
+
+        # Run through the moves, shortcutting when possible
+        best = -MATE_UPPER
+        for move, score in moves():
+            best = max(best, score)
+            if best >= gamma:
+                # Clear before setting, so we always have a value
+                if len(self.tp_move) > TABLE_SIZE: self.tp_move.clear()
+                # Save the move for pv construction and killer heuristic
+                self.tp_move[pos] = move
+                break
+
+        # Stalemate checking is a bit tricky: Say we failed low, because
+        # we can't (legally) move and so the (real) score is -infty.
+        # At the next depth we are allowed to just return r, -infty <= r < gamma,
+        # which is normally fine.
+        # However, what if gamma = -10 and we don't have any legal moves?
+        # Then the score is actaully a draw and we should fail high!
+        # Thus, if best < gamma and best < 0 we need to double check what we are doing.
+        # This doesn't prevent sunfish from making a move that results in stalemate,
+        # but only if depth == 1, so that's probably fair enough.
+        # (Btw, at depth 1 we can also mate without realizing.)
+        if best < gamma and best < 0 and depth > 0:
+            is_dead = lambda pos: any(pos.value(m) >= MATE_LOWER for m in pos.gen_moves())
+            if all(is_dead(pos.move(m)) for m in pos.gen_moves()):
+                in_check = is_dead(pos.nullmove())
+                best = -MATE_UPPER if in_check else 0
+
+        # Clear before setting, so we always have a value
+        if len(self.tp_score) > TABLE_SIZE: self.tp_score.clear()
+        # Table part 2
+        if best >= gamma:
+            self.tp_score[pos, depth, root] = Entry(best, entry.upper)
+        if best < gamma:
+            self.tp_score[pos, depth, root] = Entry(entry.lower, best)
+
+        return best
+
+    def search(self, pos, history=()):
+        """ Iterative deepening MTD-bi search """
+        self.nodes = 0
+        if DRAW_TEST:
+            self.history = set(history)
+            # print('# Clearing table due to new history')
+            self.tp_score.clear()
+
+        # In finished games, we could potentially go far enough to cause a recursion
+        # limit exception. Hence we bound the ply.
+        for depth in range(1, 1000):
+            # The inner loop is a binary search on the score of the position.
+            # Inv: lower <= score <= upper
+            # 'while lower != upper' would work, but play tests show a margin of 20 plays
+            # better.
+            lower, upper = -MATE_UPPER, MATE_UPPER
+            while lower < upper - EVAL_ROUGHNESS:
+                gamma = (lower+upper+1)//2
+                score = self.bound(pos, gamma, depth)
+                if score >= gamma:
+                    lower = score
+                if score < gamma:
+                    upper = score
+            # We want to make sure the move to play hasn't been kicked out of the table,
+            # So we make another call that must always fail high and thus produce a move.
+            self.bound(pos, lower, depth)
+            # If the game hasn't finished we can retrieve our move from the
+            # transposition table.
+            yield depth, self.tp_move.get(pos), self.tp_score.get((pos, depth, True)).lower
+
+###############################################################################
+# User interface
+###############################################################################
+
+# Python 2 compatability
+if sys.version_info[0] == 2:
+    input = raw_input
+
+
+def parse(c):
+    fil, rank = ord(c[0]) - ord('a'), int(c[1])
+    return A0 + fil - 16*rank
+
+
+def render(i):
+    rank, fil = divmod(i - A0, 16)
+    return chr(fil + ord('a')) + str(-rank)
 
 def print_pos(pos):
     print()
@@ -305,3 +445,51 @@ def print_pos(pos):
     for i, row in enumerate(pos.board.split()):
         print(' ', 9-i, ''.join(uni_pieces.get(p, p) for p in row))
     print('    ａｂｃｄｅｆｇｈｉ\n\n')
+
+def main():
+    hist = [Position(initial, 0)]
+    searcher = Searcher()
+    while True:
+        print_pos(hist[-1])
+
+        if hist[-1].score <= -MATE_LOWER:
+            print("You lost")
+            break
+
+        # We query the user until she enters a (pseudo) legal move.
+        move = None
+        while move not in hist[-1].gen_moves():
+            match = re.match('([a-i][0-9])'*2, input('Your move: '))
+            if match:
+                move = parse(match.group(1)), parse(match.group(2))
+            else:
+                # Inform the user when invalid input (e.g. "help") is entered
+                print("Please enter a move like h2e2")
+        hist.append(hist[-1].move(move))
+
+        # After our move we rotate the board and print it again.
+        # This allows us to see the effect of our move.
+        print_pos(hist[-1].rotate())
+
+        if hist[-1].score <= -MATE_LOWER:
+            print("You won")
+            break
+
+        # Fire up the engine to look for a move.
+        start = time.time()
+        for _depth, move, score in searcher.search(hist[-1], hist):
+            if time.time() - start > 1:
+                break
+
+        if score == MATE_UPPER:
+            print("Checkmate!")
+
+        # The black player moves from a rotated position, so we have to
+        # 'back rotate' the move before printing it.
+        print("Think depth: {} My move: {}".format(_depth, render(255-move[0] - 1) + render(255-move[1]-1)))
+        hist.append(hist[-1].move(move))
+
+
+if __name__ == '__main__':
+    main()
+
